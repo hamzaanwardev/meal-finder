f096597e1ec199102dcecb1962584428bd94ae7c
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-10-12 00:10:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const search = document.getElementById('search'),
   mealsEl = document.getElementById('meals'),
   resultHeading = document.getElementById('result-heading'),
   single_mealEl = document.getElementById('single-meal');
+  // Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
+  
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+  
+    // Get search term
+    const term = search.value;
+  
